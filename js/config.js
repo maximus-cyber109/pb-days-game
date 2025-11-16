f7c175e52f73e7aa9b415306beb3f81e5c71dfd0
maximus-cyber109 <scraper.updates.pinkblue@gmail.com>
2025-11-17 01:12:07 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,19 +10,9 @@ window.CR_CONFIG = {
     "Tooth-Tyrant": "https://email-editor-resources.s3.amazonaws.com/images/82618240/Tooth-Tyrant.png",
     "Quick-Cloth":  "https://email-editor-resources.s3.amazonaws.com/images/82618240/Quick-Cloth.png"
   },
-  productTier_2_4: [
-    { name: "SpeedEndo P1(2-4)", sku: "SKU1", img: "https://email-editor-resources.s3.amazonaws.com/images/82618240/NiTi-GLIDE-PATH-FILES-BOX-removebg-preview-UPDT.png" },
-    { name: "SpeedEndo P2(2-4)", sku: "SKU2", img: "https://email-editor-resources.s3.amazonaws.com/images/82618240/NiTi-GLIDE-PATH-FILES-BOX-removebg-preview-UPDT.png" },
-    // ...add up to 10
-  ],
-  productTier_5_6: [
-    { name: "SpeedEndo P1(5-6)", sku: "SKU11", img: "https://email-editor-resources.s3.amazonaws.com/images/82618240/NiTi-GLIDE-PATH-FILES-BOX-removebg-preview-UPDT.png" },
-    // ...add up to 10
-  ],
-  productTier_7: [
-    { name: "SpeedEndo P1(7)", sku: "SKU21", img: "https://email-editor-resources.s3.amazonaws.com/images/82618240/NiTi-GLIDE-PATH-FILES-BOX-removebg-preview-UPDT.png" },
-    // ...add up to 10
-  ],
+  // productTier properties are loaded dynamically from reward-logic.js
+  
+  // FIX: Using your new, working audio links from GitHub
   sounds: {
     bg: "https://raw.githubusercontent.com/maximus-cyber109/pb-days-game/main/images/mixkit-unforgiven-890.mp3",
     reveal: "https://raw.githubusercontent.com/maximus-cyber109/pb-days-game/main/images/mixkit-christmas-reveal-tones-2988.wav",
