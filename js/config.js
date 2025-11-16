d62891057a684417357fb6e0ab6b124a12a7fb41
maximus-cyber109 <scraper.updates.pinkblue@gmail.com>
2025-11-17 01:47:53 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,13 +9,8 @@ window.CR_CONFIG = {
     "Blade-Bearer": "https://email-editor-resources.s3.amazonaws.com/images/82618240/Blade-Bearer.png",
     "Tooth-Tyrant": "https://email-editor-resources.s3.amazonaws.com/images/82618240/Tooth-Tyrant.png",
     "Quick-Cloth":  "https://email-editor-resources.s3.amazonaws.com/images/82618240/Quick-Cloth.png"
-  },
+  }
   // productTier properties are loaded dynamically from reward-logic.js
   
-  // FIX: Using your new, working audio links from GitHub
-  sounds: {
-    bg: "https://raw.githubusercontent.com/maximus-cyber109/pb-days-game/main/images/mixkit-unforgiven-890.mp3",
-    reveal: "https://raw.githubusercontent.com/maximus-cyber109/pb-days-game/main/images/mixkit-christmas-reveal-tones-2988.wav",
-    redeem: "https://raw.githubusercontent.com/maximus-cyber109/pb-days-game/main/images/mixkit-magic-sweep-game-trophy-257.wav"
-  }
+  // REMOVED: No more sound properties
 };
