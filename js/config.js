7971f8ce74d73fbbdc7e94259f16f1645cd4de16
maximus-cyber109 <scraper.updates.pinkblue@gmail.com>
2025-11-17 23:06:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,13 +2,13 @@
 window.CR_CONFIG = {
   logo: "https://email-editor-resources.s3.amazonaws.com/images/82618240/Logo.png",
   cards: {
-    "LensWarden":  "https://email-editor-resources.s3.amazonaws.com/images/82618240/LensWarden.png",
-    "Device-Keeper": "https://email-editor-resources.s3.amazonaws.com/images/82618240/Device-Keeper.png",
-    "File-Forger": "https://email-editor-resources.s3.amazonaws.com/images/82618240/File-Forger.png",
-    "Crown-Shaper": "https://email-editor-resources.s3.amazonaws.com/images/82618240/Crown-Shaper.png",
-    "Blade-Bearer": "https://email-editor-resources.s3.amazonaws.com/images/82618240/Blade-Bearer.png",
-    "Tooth-Tyrant": "https://email-editor-resources.s3.amazonaws.com/images/82618240/Tooth-Tyrant.png",
-    "Quick-Cloth":  "https://email-editor-resources.s3.amazonaws.com/images/82618240/Quick-Cloth.png"
+    "LensWarden":  "https://email-editor-resources.s3.amazonaws.com/images/82618240/CARDS/LensWarden.png",
+    "Device-Keeper": "https://email-editor-resources.s3.amazonaws.com/images/82618240/CARDS/Device-Keeper.png",
+    "File-Forger": "https://email-editor-resources.s3.amazonaws.com/images/82618240/CARDS/File-Forger.png",
+    "Crown-Shaper": "https://email-editor-resources.s3.amazonaws.com/images/82618240/CARDS/Crown-Shaper.png",
+    "Blade-Bearer": "https://email-editor-resources.s3.amazonaws.com/images/82618240/CARDS/Blade-Bearer.png",
+    "Tooth-Tyrant": "https://email-editor-resources.s3.amazonaws.com/images/82618240/CARDS/Tooth-Tyrant.png",
+    "Quick-Cloth":  "https://email-editor-resources.s3.amazonaws.com/images/82618240/CARDS/Quick-Cloth.png"
   }
   // productTier properties are loaded dynamically from reward-logic.js
   
